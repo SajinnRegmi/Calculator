c45e8d1ff9496690477bf9d60faf2e72d1eac4e7
SajinnRegmi <regmisazeen4@gmail.com>
2025-06-16 10:45:00 +0545

    change to calculoter

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Change from "./parts/Change";
+import NewCalcu from "./parts/NewCalcu";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="h-[100vh]">
-      <Change />
+      <NewCalcu />
     </div>
   );
 }
